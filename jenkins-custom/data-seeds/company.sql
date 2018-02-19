4622df15a76ce766641e3a14ea09261b2ca7189e
hans-van-der-linden <36532588+hans-van-der-linden@users.noreply.github.com>
2018-02-19 10:39:12 +0100

    Create company.sql

---
+++ jenkins-custom/data-seeds/company.sql
@@ -0,0 +1,5 @@
+INSERT INTO company (company_id, name) VALUES (1, 'Acme Corporation');
+INSERT INTO company (company_id, name) VALUES (2, 'Globex Corporation');
+INSERT INTO company (company_id, name) VALUES (3, 'Soylent Corp');
+INSERT INTO company (company_id, name) VALUES (4, 'Initech');
+INSERT INTO company (company_id, name) VALUES (5, 'Massive Dynamic');
