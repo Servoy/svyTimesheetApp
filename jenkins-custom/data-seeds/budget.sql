02ab62d5f952c13c9d115bb8618834ff6e743484
hans-van-der-linden <36532588+hans-van-der-linden@users.noreply.github.com>
2018-02-19 10:40:03 +0100

    Create budget.sql

---
+++ jenkins-custom/data-seeds/budget.sql
@@ -0,0 +1,15 @@
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (3, 1, 'Budget March 2018', 100, 100, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (5, 2, 'Debugging', 50, 50, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (6, 2, 'Assistance', 20, 20, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (7, 2, 'Meetings', 40, 40, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (1, 1, 'Budget January 2018', 80, 80, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (4, 1, 'Budget April 2018', 200, 200, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (8, 3, 'Budget 1', 200, 200, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (9, 3, 'Budget 2', 100, 100, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (10, 3, 'Budget 3', 400, 400, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (11, 4, 'Development', 250, 250, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (12, 4, 'Testing', 50, 50, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (13, 4, 'Planning', 20, 20, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (15, 5, 'Budget B', 400, 400, 0, 0);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (14, 5, 'Budget A', 300, 300, 0, 8);
+INSERT INTO budget (budget_id, company_id, name, quantity, hours_remaining, hours_spent_billable, hours_spent_total) VALUES (2, 1, 'Budget February 2018', 60, 60, 0, 0);
